27eec0b5b44e48f2a3e60d1e32829dea5a76580d
Ashaaf92 <ashaaf92@gmail.com>
2026-06-07 16:57:01 +0530

    add Inngest client and order placement function with Express server integration

---
+++ 11-inngest-ai/server.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+
+import express from "express";
+import {serve} from "inngest/express";
+import {inngest} from "./inngest-client.js";
+import { onOrderPlaced} from "./01-inngest.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/inngest", serve(
+    {
+        client: inngest,
+        functions: [onOrderPlaced],
+    }),
+);
+
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
